b5f078cad5248ff1aae002152b44bd73b308ccf6
Bakhoumalbert <bakhoum.albert@uam.edu.sn>
2023-02-18 19:43:42 +0000

    mise a jour

--- app.py
+++ app.py
@@ -14,9 +14,9 @@ class Todo(db.Model):
     title = db.Column(db.String(100))
     complete = db.Column(db.Boolean)
 
-
-db.create_all()
-
+@app.before_first_request
+def create_tables():
+    db.create_all()
 
 @app.get("/")
 def home():
